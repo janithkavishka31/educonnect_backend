2ac0b4850f4d35b54075f2f06a4ddae4f70e2da5
janithkavishka31 <160833581+janithkavishka31@users.noreply.github.com>
2025-12-27 13:55:41 +0530

    Initial backend project setup

---
+++ src/main/java/com/example/educonnect_platform/EduconnectPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.educonnect_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduconnectPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduconnectPlatformApplication.class, args);
+	}
+
+}
